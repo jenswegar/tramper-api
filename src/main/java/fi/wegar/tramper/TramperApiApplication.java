e278966e05c7d672a215cdefeb8c34f5993b67bb
Jens Wegar <jens.wegar@gmail.com>
2020-02-05 23:48:21 +0200

    initial commit

---
+++ src/main/java/fi/wegar/tramper/TramperApiApplication.java
@@ -0,0 +1,13 @@
+package fi.wegar.tramper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/wegar/tramper/TramperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.wegar.tramper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
